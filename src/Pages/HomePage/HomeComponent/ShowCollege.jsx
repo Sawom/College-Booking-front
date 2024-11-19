afb8f0ef1eef313258dec0080e372020001f542f
Sawom <asawom250@gmail.com>
2024-11-20 00:54:02 +0600

    clg show

---
+++ src/Pages/HomePage/HomeComponent/ShowCollege.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ShowCollege = () => {
+    return (
+        <div className='container mx-auto'>
+            {/* search */}
+            <div>
+
+            </div>
+            {/* show college */}
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default ShowCollege;
