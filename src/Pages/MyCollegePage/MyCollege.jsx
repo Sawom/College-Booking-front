7fe7304c7a07a60f824577c8f98c2c6e8d3ae5bd
Sawom <asawom250@gmail.com>
2024-11-21 03:25:59 +0600

    post reviews

--- src/Pages/MyCollegePage/MyCollege.jsx
+++ src/Pages/MyCollegePage/MyCollege.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import useAuth from "../../Authentication/useAuth/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const MyCollege = () => {
   const { user, loading, setLoading } = useAuth();
   const [admissionData, setAdmissionData] = useState([]);
+
+  const { register, handleSubmit, reset } = useForm();
+  const [rating, setRating] = useState(null);
 
   // load home college data
   useEffect(() => {
@@ -20,13 +26,35 @@ const MyCollege = () => {
     }
   }, [user]);
 
+   // add review
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    const { collegeName, name, review, rating } = data;
+    axios.post(
+        "http://localhost:5000/reviews",
+        data
+      )
+      .then((data) => {
+        if (data.data.insertedId) {
+          reset();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Thanks for your review",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        }
+      });
+  };
+
   return (
     <div className="mt-10">
       <div className="container mx-auto ">
         <p className="font-bold text-center text-2xl"> Admission Profile </p>
 
         {admissionData.map((data, index) => (
-          <div className="grid lg:grid-cols-2 mb-10 md:grid-cols-2 grid-cols-1 w-full shadow-2xl p-3 gap-4">
+          <div className="grid lg:grid-cols-2 mb-10 md:grid-cols-2 grid-cols-1 w-full shadow-xl p-3 gap-4">
             {/* 1 */}
             <div>
               <figure>
@@ -39,17 +67,138 @@ const MyCollege = () => {
             </div>
             {/* 2 */}
             <div>
-                <p> <strong>College Name:</strong> {data.collegeName} </p> 
-                <p> <strong>Candidate Name:</strong> {data.name} </p>
-                <p> <strong>Email:</strong> {data.email} </p>
-                <p> <strong>Subject</strong> {data.subject} </p>
-                <p> <strong>Phone</strong> {data.phone} </p>
-                <p> <strong>Address</strong> {data.address} </p>
-                <p> <strong>Date of Birth: </strong> {data.dob} </p>
-
+              <p>
+                <strong>College Name:</strong> {data.collegeName}
+              </p>
+              <p>
+                <strong>Candidate Name:</strong> {data.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {data.email}
+              </p>
+              <p>
+                <strong>Subject</strong> {data.subject}
+              </p>
+              <p>
+                <strong>Phone</strong> {data.phone}
+              </p>
+              <p>
+                <strong>Address</strong> {data.address}
+              </p>
+              <p>
+                <strong>Date of Birth: </strong> {data.dob}
+              </p>
             </div>
           </div>
         ))}
+
+        {/* review form */}
+        <div className="container mx-auto mt-4 mb-4 flex justify-center items-center">
+          <div className="card w-full max-w-lg shrink-0 p-5 shadow-xl">
+            <h2 className=" text-center font-bold text-2xl"> Your review </h2>
+            {/* form */}
+            <form className="card-body"  onSubmit={handleSubmit(onSubmit)} >
+              {/* name */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Reviewed by:</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="your name"
+                  className="input input-bordered"
+                  name="name"
+                  defaultValue={""}
+                  {...register("name", { required: true })}
+                />
+              </div>
+              {/* college name */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">College Name:</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="College Name"
+                  className="input input-bordered"
+                  name="collegeName"
+                  defaultValue={""}
+                  {...register("collegeName", { required: true })}
+                />
+              </div>
+              {/* your review */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your review:</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Your review"
+                  className="input input-bordered"
+                  name="review"
+                  defaultValue={""}
+                  {...register("review", { required: true })}
+                />
+              </div>
+              {/* ratings */}
+              <div className="form-control rating mb-5">
+              <label className="label">
+                <span className="label-text">Ratings:</span>
+              </label>
+              {/* Radio buttons for 5-star rating */}
+              <div className="flex">
+                <input
+                  type="radio"
+                  name="rating"
+                  value="1"
+                  className="mask mask-star-2 bg-green-500"
+                  {...register("rating", { required: true })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="2"
+                  className="mask mask-star-2 bg-green-500"
+                  {...register("rating", { required: true })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="3"
+                  className="mask mask-star-2 bg-green-500"
+                  {...register("rating", { required: true })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="4"
+                  className="mask mask-star-2 bg-green-500"
+                  {...register("rating", { required: true })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="5"
+                  className="mask mask-star-2 bg-green-500"
+                  {...register("rating", { required: true })}
+                />
+              </div>
+            </div>
+
+            {/* button */}
+            <button
+                className="btn "
+                style={{
+                  backgroundColor: "#016A4E",
+                  color: "white",
+                  fontStyle: "bold",
+                }}>
+                Submit
+              </button>
+
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
